310acf7a7295057b0ba4c119c8016dc56a0e847c
FSF0912 <fsf_out@outlook.com>
2025-01-15 17:17:42 +0800

    Update Pop-up interface

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,7 +1,14 @@
 using System;
+using FSF.CollectionFrame;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-
+    public PopupInterface popupInterface;
+    private void Start() {
+        GetComponent<Button>().onClick.AddListener(()=>{
+            popupInterface.OperatePanel(this.transform.position,true);
+        });
+    }
 }
